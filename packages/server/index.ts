1cf03be114e2a4f874af6ed20237511434f62a6e
annekurian <37437111+annekurian@users.noreply.github.com>
2026-07-09 12:34:02 +0530

    Log server start port to console

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -12,6 +12,7 @@ app.use(cors({
 app.use(express.json());
 app.use(router);
 const port = process.env.PORT || 3000;
+console.log(`Starting on port: ${port}`);
 
 app.listen(port, '0.0.0.0', () => {
   console.log(`Server is running on http://localhost:${port}`);
